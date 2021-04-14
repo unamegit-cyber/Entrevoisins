b941754d15abc0c433cad414d0491bc31bbfb328
unamegit-cyber <chevet.guillaume@gmail.com>
2021-04-14 23:48:27 +0200

    Open Detail activity Neighbour

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
@@ -3,6 +3,9 @@ package com.openclassrooms.entrevoisins.ui.neighbour_list;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -34,10 +37,21 @@ public class DetailNeighbourActivity extends AppCompatActivity {
     TextView mDetailName;
     @BindView(R.id.item_detail_favoris_button)
     ImageButton mDetailFavorisButton;
+    @BindView(R.id.item_detail_name2)
+    TextView mDetailName2;
+    @BindView(R.id.item_detail_adresse)
+    TextView mDetailAdresse;
+    @BindView(R.id.item_detail_telephone)
+    TextView mDetailTelephone;
+    @BindView(R.id.item_detail_web)
+    TextView mDetailWeb;
+    @BindView(R.id.item_detail_description)
+    TextView mDetailDescription;
 
     private NeighbourApiService mApiService;
     private List<Neighbour> mNeighbours;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +64,11 @@ public class DetailNeighbourActivity extends AppCompatActivity {
         mDetailAvatar = (ImageView) findViewById(R.id.item_detail_avatar);
         mDetailName = (TextView) findViewById(R.id.item_detail_name);
         mDetailFavorisButton = (ImageButton) findViewById(R.id.item_detail_favoris_button);
+        mDetailName2 = (TextView) findViewById(R.id.item_detail_name2);
+        mDetailAdresse = (TextView) findViewById(R.id.item_detail_adresse);
+        mDetailTelephone = (TextView) findViewById(R.id.item_detail_telephone);
+        mDetailWeb = (TextView) findViewById(R.id.item_detail_web);
+        mDetailDescription = (TextView) findViewById(R.id.item_detail_description);
 
         Intent intent = getIntent();
         int neighbour_position = Integer.parseInt(intent.getStringExtra("neighbour_position"));
@@ -59,6 +78,13 @@ public class DetailNeighbourActivity extends AppCompatActivity {
         Neighbour neighbour = mNeighbours.get(neighbour_position);
 
         mDetailName.setText(neighbour.getName());
+        mDetailName2.setText(neighbour.getName());
+        mDetailAdresse.setText(neighbour.getAddress());
+        mDetailTelephone.setText(neighbour.getPhoneNumber());
+        mDetailWeb.setText("www.facebook.fr/" + neighbour.getName().toLowerCase());
+        mDetailDescription.setText(neighbour.getAboutMe());
+
+        mDetailFavorisButton.setImageResource(R.drawable.ic_baseline_star_yellow_24);
 
         Glide.with(this)
                 .load(neighbour.getAvatarUrl())
@@ -66,6 +92,7 @@ public class DetailNeighbourActivity extends AppCompatActivity {
                 .into(mDetailAvatar);
 
     }
+
 /*
     public View onCreateView(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
