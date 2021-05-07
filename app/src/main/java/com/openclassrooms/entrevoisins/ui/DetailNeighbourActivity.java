0d301f48c5925ee8a0c47d10761eb772d26ee287
Guillaume CHEVET <chevet.guillaume@gmail.com>
2021-05-07 22:17:04 +0200

    nettoyage du code

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/DetailNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/DetailNeighbourActivity.java
@@ -1,15 +1,9 @@
 package com.openclassrooms.entrevoisins.ui;
 
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
